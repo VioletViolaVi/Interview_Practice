51cacb4c7758ff709559144d344f9936b434713b
VioletViolaVi <vivian.v18723@gmail.com>
2021-03-07 00:36:47 +0000

    Interview practice.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // FizzBuzz: for loop
-  for (let i = 0; i < 101; i++) {
-    if (i !== 0 && i % 3 === 0 && i % 5 === 0) {
-      console.log(i + ": FizzBuzz");
-    } else if (i !== 0 && i % 3 === 0) {
-      console.log(i + ": Fizz");
-    } else if (i !== 0 && i % 5 === 0) {
-      console.log(i + ": Buzz");
-    } else {
-      console.log(i);
-    }
-  }
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   // FizzBuzz: for loop
+//   for (let i = 0; i < 101; i++) {
+//     if (i !== 0 && i % 3 === 0 && i % 5 === 0) {
+//       console.log(i + ": FizzBuzz");
+//     } else if (i !== 0 && i % 3 === 0) {
+//       console.log(i + ": Fizz");
+//     } else if (i !== 0 && i % 5 === 0) {
+//       console.log(i + ": Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// });
+// $("document").ready(function () {
+
+//   $("a").click(function () {
+//     $("li").toggle();
+//   });
+// });
